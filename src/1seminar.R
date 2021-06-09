7b1852c57f0f79f8cd3c7f7e0998a72ad9093534
WhiteTeaDragon <AlexSend57@gmail.com>
2021-06-09 19:21:12 +0300

    new graph for g4

--- src/1seminar.R
+++ src/1seminar.R
@@ -9,8 +9,8 @@ setwd("C:/Users/Alexandra/Documents/Биоинформатика/final_project")
 # NAME <- 'H3K36me3_H7.ENCFF063DDB.hg38'
 # NAME <- 'H3K36me3_H7.ENCFF864HTI.hg19'
 # NAME <- 'H3K36me3_H7.ENCFF864HTI.hg38'
-# NAME <- 'g4.merged'
-NAME <- 'exp.g4.intersection'
+NAME <- 'g4.merged'
+# NAME <- 'exp.g4.intersection'
 OUT_DIR <- 'Results/'
 
 ###
@@ -20,6 +20,20 @@ bed_df <- read.delim(paste0(NAME, '.bed'), as.is = TRUE, header = FALSE)
 colnames(bed_df) <- c('chrom', 'start', 'end')
 bed_df$len <- bed_df$end - bed_df$start
 head(bed_df)
+
+# bed_df <- bed_df %>%
+#   arrange(-len) %>%
+#   filter(len < 400)
+# 
+# ggplot(bed_df) +
+#   aes(x = len) +
+#   geom_histogram() +
+#   ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+#   theme_bw()
+# 
+# 
+# ggsave(paste0('len_hist.', NAME, '.filtered.png'), path = OUT_DIR)
+
 
 ggplot(bed_df) +
   aes(x = len) +
